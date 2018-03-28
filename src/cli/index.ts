9c50f5ec09089487ae56a2c538fcba2f81e8f319
Andrew Mast <andrew@andrewmast.com>
2018-03-27 21:18:31 -0500

    Fixed yet another error

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,20 +1,18 @@
 import { Cli } from './cli';
 
 let cli = new Cli();
-let yargs = require('yargs')();
+
+let yargs = require('yargs')
+    .usage("Usage: laravel-echo-server <command> [options]")
+    .command("start",               "Start up the server.",                          args => cli.start(args))
+    .command("stop",                "Stops the server.",                             args => cli.stop(args))
+    .command(["configure", "init"], "Initialize server with a config file.",         args => cli.configure(args)) // Has an alias of "init" for backwards compatibility, remove in next version
+    .command("client:add",          "Register a client that can make api requests.", args => cli.clientAdd(args))
+    .command("client:remove",       "Remove a registered client.",                   args => cli.clientRemove(args))
+    .demand(1, "Please provide a valid command.")
+    .help("help")
+    .alias("help", "h");
 
 yargs.$0 = '';
 
-/**
- * CLI Commands.
- */
-var argv = yargs.usage("Usage: laravel-echo-server <command> [options]")
-    .command("start",               "Start up the server.",                          yargs => cli.start(yargs))
-    .command("stop",                "Stops the server.",                             yargs => cli.stop(yargs))
-    .command(["configure", "init"], "Initialize server with a config file.",         yargs => cli.configure(yargs)) // Has an alias of "init" for backwards compatibility, remove in next version
-    .command("client:add",          "Register a client that can make api requests.", yargs => cli.clientAdd(yargs))
-    .command("client:remove",       "Remove a registered client.",                   yargs => cli.clientRemove(yargs))
-    .demand(1, "Please provide a valid command.")
-    .help("help")
-    .alias("help", "h")
-    .argv;
+var argv = yargs.argv;
