d4bd3bd9b4dc4ee2781bf9e33d1b36414a51d5fc
Favre Anael <anael.favre@gmail.com>
2018-03-29 18:39:48 +0200

    Create a simple plugin handler

---
+++ src/plugins.ts
@@ -0,0 +1,54 @@
+import { EventEmitter } from 'events';
+import { Log } from './log';
+
+export class Plugins {
+    /**
+     * The singleton instance of this object.
+     *
+     * @type {EventEmitter}
+     */
+    private static instance: Plugins;
+
+    /**
+     * The event handler for hooking plugins events.
+     *
+     * @type {EventEmitter}
+     */
+    public events: EventEmitter;
+
+    /**
+     * The plugins instances.
+     *
+     * @type {Array}
+     */
+    private all: Array<Object>;
+
+    /**
+     * Create a new instance.
+     */
+    public constructor(plugins) {
+        Plugins.instance = this;
+
+        this.events = new EventEmitter();
+        this.all = [];
+
+        if(plugins.length === 0) {
+            Log.success('No plugins to be loaded')
+        } else {
+            for (let i = 0; i < plugins.length; i++) {
+                Log.success('Loading plugin : ' + plugins[i] + ' ...');
+                try {
+                    let plugin = require(plugins[i]);
+                    plugin.install(this.events);
+                    this.all.push(plugin)
+                } catch (e) {
+                    Log.error(e);
+                }
+            }
+        }
+    }
+
+    static emit(event, options?): void {
+        this.instance.events.emit(event, options)
+    }
+}
