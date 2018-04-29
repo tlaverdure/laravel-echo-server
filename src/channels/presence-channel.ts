ff019029970bf6b697ac9c2d3cb57acad6288e69
Pierre Borgmann <drweissbrot@outlook.com>
2018-04-29 10:08:14 +0200

    fix broadcasting of presence channel events

--- src/channels/presence-channel.ts
+++ src/channels/presence-channel.ts
@@ -105,7 +105,7 @@ export class PresenceChannel {
 
                 this.onSubscribed(socket, channel, members);
 
-                if (!is_member) {
+                if (is_member) {
                     this.onJoin(socket, channel, member);
                 }
             }, error => Log.error(error));
@@ -131,7 +131,7 @@ export class PresenceChannel {
             this.db.set(channel + ':members', members);
 
             this.isMember(channel, member).then(is_member => {
-                if (!is_member) {
+                if (is_member) {
                     delete member.socketId;
                     this.onLeave(channel, member);
                 }
