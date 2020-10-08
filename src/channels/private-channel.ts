714a889478cabc68cc27faabb94a9b5f45b2cad5
Hugues Joyal <hjoyal@webo3.com>
2020-10-08 06:56:22 -0400

    Add the user-agent and the remote address ip

--- src/channels/private-channel.ts
+++ src/channels/private-channel.ts
@@ -122,6 +122,8 @@ export class PrivateChannel {
     protected prepareHeaders(socket: any, options: any): any {
         options.headers['Cookie'] = options.headers['Cookie'] || socket.request.headers.cookie;
         options.headers['X-Requested-With'] = 'XMLHttpRequest';
+        options.headers["User-Agent"] = socket.request.headers["user-agent"];
+        options.headers["X-Forwarded-For"] = socket.request.headers["x-forwarded-for"] || socket.conn.remoteAddress;
 
         return options.headers;
     }
